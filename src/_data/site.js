7fc632f7c52e3e3a91dab7b617e65fc9933ab123
Harvey Ramer <harvey@harveyramer.com>
2021-05-31 14:25:35 -0500

    Splash page.

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "title": "Web Starter",
-  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
-  "url": "https://webstarter.chriscollins.me",
-  "author": "Chris Collins",
+  "title": "ARC Waste Compactors",
+  "description": "ARC makes waste equipment hassle-free with high-quality equipment and best-in-class service.",
+  "url": "https://www.flaremark.com",
+  "author": "FlareMark",
   "meta_data": {
-    "twitter": "@scottishstoater",
-    "default_social_image": "/static/default_social_image.jpg"
+    "twitter": "@flaremarkagency",
+    "default_social_image": "/static/default_social_image.png"
   },
   "env": process.env.ELEVENTY_ENV === 'production'
 }
