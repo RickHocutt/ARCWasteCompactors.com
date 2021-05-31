67b148a95c633adbf1e4a97d37b553774497c0ee
Harvey Ramer <harvey@harveyramer.com>
2021-05-31 14:57:11 -0500

    Fix canonical URL.

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
   "title": "ARC Waste Compactors",
   "description": "ARC makes waste equipment hassle-free with high-quality equipment and best-in-class service.",
-  "url": "https://www.flaremark.com",
-  "author": "FlareMark",
+  "url": "https://www.arcwastecompactors.com",
+  "author": "ARC Waste Compactors",
   "meta_data": {
     "twitter": "@flaremarkagency",
     "default_social_image": "/static/default_social_image.png"
